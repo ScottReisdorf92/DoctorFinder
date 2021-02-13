2539016e62c1bac785771577ecc3830585e0aa27
= <scottdreisdorf@gmail.com>
2021-02-13 14:12:50 -0600

    Added cookies and finished doctor login methods

--- src/main/java/com/revature/services/DoctorServImpl.java
+++ src/main/java/com/revature/services/DoctorServImpl.java
@@ -1,7 +1,11 @@
 package com.revature.services;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.revature.entities.Doctor;
 import com.revature.repositories.DoctorRepo;
@@ -17,8 +21,16 @@ public class DoctorServImpl implements DoctorServ {
 		Doctor foundDoc = dr.getDoctorByEmail(doctor.getEmail());
 		if (foundDoc != null && foundDoc.getPassword().equals(doctor.getPassword())) {
 			System.out.println(foundDoc);
+			
 			return foundDoc;
 		}
 		return null;
 	}
+
+	@Override
+	public Doctor loggedInDoctor(String email) {
+		return dr.getDoctorByEmail(email);
+	}
+	
+	
 }
